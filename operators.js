12b5c9ba6412f34fa4537538231813171cbf4b91
Anupa Rishendra <150948367+Rishendra7@users.noreply.github.com>
2025-01-21 10:32:58 +0530

    Update operators.js

--- operators.js
+++ operators.js
@@ -62,3 +62,40 @@
 
 // console.log(!true);
 // console.log(!false);
+
+
+// console.log(10);
+// console.log(20);
+// console.log(30);
+// setTimeout(() => {
+//     console.lOg(40);
+// }, 4000);
+// console.log(50);
+// console.log(60);
+
+
+//Promise
+let p1 = new Promise((resolve , reject) => {});
+console.log(p1);
+
+let p2 = new Promise((resolve , reject) => {
+    resolve('Success');
+});
+console.log(p2);
+p2.then((response) => {
+    console.log(response);
+}).catch((error) => {
+    console.log(error);
+}).finally(() =>
+    console.log('finally prinitng for both success and failure'))
+
+let p3 = new Promise((resolve , reject) => {
+    reject('Failed');
+});
+console.log(p3);
+p3.then((response) => {
+    console.log(response);
+}).catch((error) => {
+    console.log(error);
+}).finally(() =>
+    console.log('finally prinitng for both success and failure'))
