d2e85ac7c6f7a6a45355943505aae890ab2b3c6e
Anupa Rishendra <150948367+Rishendra7@users.noreply.github.com>
2025-01-23 10:31:58 +0530

    Update async.js

--- async.js
+++ async.js
@@ -62,3 +62,26 @@ function fetchUsers(){
     })
 }
 fetchUsers();
+
+
+let p = new Promise((resolve , reject) => {
+    setTimeout(() => {
+        resolve('Promise successfully completing');
+    }, 3000);
+})
+
+async function demo(){
+    console.log("Start");
+    let x = await p;
+    console.log(x);
+
+    console.log("End");
+}
+demo();
+
+async function fetchUsers(){
+    let respose = await fetch("https://jsonplaceholder.typicode.com/users");
+    let data = await respose.json();
+    console.log(data);
+}
+fetchUsers();
