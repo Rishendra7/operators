59ff2354d1f5a95f03f5dd2dd0c3a2e48db8cf98
Anupa Rishendra <150948367+Rishendra7@users.noreply.github.com>
2025-01-23 10:30:17 +0530

    Add files via upload

---
+++ dom1.js
@@ -0,0 +1,7 @@
+let element = document.getElementById("demo");
+element.innerText = "DOM";
+console.log(element);
+
+let div = document.getElementById("test");
+div.innerHTML = "<h1>Header</h1>"
+console.log(div);
